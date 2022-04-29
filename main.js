036dd5fbea6ff2b14136a275a3d042aa3bc13679
Lorentz Bloom <lorentzbloomguitar@gmail.com>
2022-04-29 14:16:34 +0100

    wip

--- main.js
+++ main.js
@@ -152,66 +152,65 @@ while (userName === undefined) {
   }
   if (userName.charAt(0).match(/[A-Z]+/)) {
     console.log("Username is valid");
-    
-  } 
-    else {
+  } else {
     console.log("Username not valid");
     alert("Username must start with capital letter!");
     userName = undefined;
-      }
-    }
-  while (keepLooping === true && userName) {
-    playerMove = prompt("What is your move?");
+  }
+}
+while (keepLooping === true && userName) {
+  playerMove = prompt("What is your move?");
 
-    function scores() {
-      return `Win count: ${playerWin}
+  function scores() {
+    return `Win count: ${playerWin}
   Loss count: ${playerLoss}
   Draw count: ${playerDraw}
   Number of Rounds: ${roundNumber}
   `;
-    }
+  }
 
-    function getWinner(playerMove, computerMove) {
-      if (playerMove === computerMove) {
-        roundNumber++;
-        playerDraw++;
-        return alert("Hey " + userName + ", you drew!\n" + scores());
-      } else {
-        if (playerMove === "rock" && computerMove === "scissors") {
-          roundNumber++;
-          playerWin++;
-          return alert("Hey " + userName + ", you win!\n" + scores());
-        } else if (playerMove === "paper" && computerMove === "rock") {
-          roundNumber++;
-          playerWin++;
-          return alert("Hey " + userName + ", you win!\n" + scores());
-        } else if (playerMove === "scissors" && computerMove === "paper") {
-          roundNumber++;
-          playerWin++;
-          return alert("Hey " + userName + ", you win!\n" + scores());
-        } else {
-          roundNumber++;
-          playerLoss++;
-          return alert("Hey " + userName + ", you lost!\n" + scores());
-        }
-      }
-    }
+  // substantial change
 
-    // Computer move
-
-    function randomArray() {
-      let arr = ["rock", "paper", "scissors"];
-      return arr[Math.floor(Math.random() * arr.length)];
-    }
-
-    computerMove = randomArray();
-
-    let result = getWinner(playerMove, computerMove);
-
-    if (confirm("Do you want to play again?") == true) {
-      keepLooping = true;
+  function getWinner(playerMove, computerMove) {
+    if (playerMove === computerMove) {
+      roundNumber++;
+      playerDraw++;
+      return alert("Hey " + userName + ", you drew!\n" + scores());
     } else {
-      keepLooping = false;
+      if (playerMove === "rock" && computerMove === "scissors") {
+        roundNumber++;
+        playerWin++;
+        return alert("Hey " + userName + ", you win!\n" + scores());
+      } else if (playerMove === "paper" && computerMove === "rock") {
+        roundNumber++;
+        playerWin++;
+        return alert("Hey " + userName + ", you win!\n" + scores());
+      } else if (playerMove === "scissors" && computerMove === "paper") {
+        roundNumber++;
+        playerWin++;
+        return alert("Hey " + userName + ", you win!\n" + scores());
+      } else {
+        roundNumber++;
+        playerLoss++;
+        return alert("Hey " + userName + ", you lost!\n" + scores());
+      }
     }
   }
 
+  // Computer move
+
+  function randomArray() {
+    let arr = ["rock", "paper", "scissors"];
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  computerMove = randomArray();
+
+  let result = getWinner(playerMove, computerMove);
+
+  if (confirm("Do you want to play again?") == true) {
+    keepLooping = true;
+  } else {
+    keepLooping = false;
+  }
+}
